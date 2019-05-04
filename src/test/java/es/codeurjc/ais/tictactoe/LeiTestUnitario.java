27eebf3aab2e6a2b61a3a95ba5869967564df9c3
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-05-04 16:30:07 +0200

    lei test

--- src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
+++ src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
@@ -1,5 +1,19 @@
 package es.codeurjc.ais.tictactoe;
 
-public class LeiTestUnitario {
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
+public class LeiTestUnitario {
+	@BeforeClass
+	public static void setupTest() {
+		
+	}
+	@AfterClass
+	public static void teardown() {
+		
+	}
+	@Before
+	public void setup() {
+		
+	}
 }
