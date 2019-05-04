1556ea53aa206452ee35a91ce93c54c4a74f7178
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-05-04 17:58:11 +0200

    a

--- src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
+++ src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
@@ -29,7 +29,22 @@ public class LeiTestUnitario {
 		
 	}
 	@Test
-	public void drawTest() {
+	public void trueDrawTest() {
+		//given
+		//dato dado
+		/* X 0 X
+		 * 0 X X
+		 * 0 X 0
+		 * */
+		asignarValores("X0X0XX0X0");
+		//when
+		boolean draw=tablero.checkDraw();
+		//then
+		assertThat(draw,is(true));
+	}
+
+	@Test
+	public void falseDrawTest(){
 		//given
 		//dato dado
 		/* X 0 X
@@ -37,13 +52,17 @@ public class LeiTestUnitario {
 		 * 0 X 0
 		 * */
 		tablero.getCell(0).value="X";tablero.getCell(1).value="0";tablero.getCell(2).value="X";
-				
+
 		tablero.getCell(3).value="0";tablero.getCell(4).value="X";tablero.getCell(5).value="X";
-		
+
 		tablero.getCell(6).value="0";tablero.getCell(7).value="X";tablero.getCell(8).value="0";
-		//when
-		boolean draw=tablero.checkDraw();
-		//then
-		assertThat(draw,is(true));
+
+
+	}
+
+	private void asignarValores(String letras){
+		for(int i=0;i<letras.length();i++){
+			tablero.getCell(i).value=Character.toString(letras.charAt(i));
+		}
 	}
 }
