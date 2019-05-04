e4e716b160d763c98a690014c3eceb5533b1ef47
vlt23 <vlt23@protonmail.com>
2019-05-04 16:29:19 +0200

    ...

---
+++ src/test/java/es/codeurjc/ais/tictactoe/Valen.java
@@ -0,0 +1,12 @@
+package es.codeurjc.ais.tictactoe;
+
+import org.junit.Test;
+
+public class Valen {
+
+    @Test
+    public void boardUnitTest() {
+        Board board =  new Board();
+    }
+
+}
