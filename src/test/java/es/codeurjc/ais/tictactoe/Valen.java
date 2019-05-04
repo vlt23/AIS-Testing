86c95f94c67ce65cb441777d5036e3a818739ee7
vlt23 <vlt23@protonmail.com>
2019-05-04 17:34:54 +0200

    ADD empate test

--- src/test/java/es/codeurjc/ais/tictactoe/Valen.java
+++ src/test/java/es/codeurjc/ais/tictactoe/Valen.java
@@ -1,12 +1,42 @@
 package es.codeurjc.ais.tictactoe;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class Valen {
 
+    private Board board;
+
+    @Before
+    public void setUp() {
+        WebApp.start();
+        board = new Board();
+    }
+
+    @After
+    public void shutdown() {
+        WebApp.stop();
+    }
+
     @Test
-    public void boardUnitTest() {
-        Board board =  new Board();
+    public void empate() {
+        /* X O X
+           O X O
+           X O X
+         */
+        for (int i = 0; i < 9; i++) {
+            TicTacToeGame.Cell cell = board.getCell(i);
+            if (i % 2 == 0) {
+                cell.value = "X";
+            } else {
+                cell.value = "O";
+            }
+        }
+        assertThat(board.checkDraw(), is(true));
     }
 
 }
