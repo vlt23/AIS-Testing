d576f8b28efe3549a9ecbafb1738e5d69f0d9ec4
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-05-04 17:16:20 +0200

    class lei

--- src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
+++ src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
@@ -1,9 +1,15 @@
 package es.codeurjc.ais.tictactoe;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class LeiTestUnitario {
+	private Board tablero;
+	
 	@BeforeClass
 	public static void setupTest() {
 		
@@ -14,6 +20,28 @@ public class LeiTestUnitario {
 	}
 	@Before
 	public void setup() {
+		tablero=new Board();
+		tablero.checkDraw();
+	}
+	@After
+	public void close() {
 		
+	}
+	@Test
+	public void drawTest() {
+		//given
+		/* X 0 X
+		 * 0 X X
+		 * 0 X 0
+		 * */
+		tablero.getCell(0).value="X";tablero.getCell(1).value="0";tablero.getCell(2).value="X";
+				
+		tablero.getCell(3).value="0";tablero.getCell(4).value="X";tablero.getCell(5).value="X";
+		
+		tablero.getCell(6).value="0";tablero.getCell(7).value="X";tablero.getCell(8).value="0";
+		//when
+		boolean draw=tablero.checkDraw();
+		//then
+		assertThat(tablero.checkDraw(),equals(true));
 	}
 }
