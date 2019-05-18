ab6927813fcf0897f4afd4c3b3176e541fb206a7
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-05-18 20:00:53 +0200

    a

--- src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
+++ src/test/java/es/codeurjc/ais/tictactoe/LeiTestUnitario.java
@@ -41,7 +41,7 @@ public class LeiTestUnitario {
     //POR SI SE USA DIRECTAMENTE NO PUEDE COMPROABAR EL EMPATE
     //FALSE DRAW
     @Test
-    public void FalseCheckDrawTest() {
+    public void falseCheckDrawTest() {
     	//given
         /* X 0 X
            0 X 0
@@ -53,10 +53,10 @@ public class LeiTestUnitario {
         //then
         assertThat(draw, is(false));
     }
-
     
+    //TEST P1 GANA Y P2 PIERDE    
     @Test
-    public void trueP1WinAndP2LoseTest() {
+    public void getCellsIfWinneP1WinAndP2LoseTest() {
     	//given
         /* X X X
            0 0 X
@@ -72,9 +72,9 @@ public class LeiTestUnitario {
         assertThat(cellsWinner, equalTo(expectedCells));
         assertThat(cellsLoser,equalTo(null));
     }
-    
+    //TEST P1 PIERDA Y P2 GANA
     @Test
-    public void trueP1LoseAndP2WinTest() {
+    public void getCellsIfWinnerP1LoseAndP2WinTest() {
     	//given
         /* 0 0 0
            X X 0
@@ -90,7 +90,41 @@ public class LeiTestUnitario {
         assertThat(cellsWinner, equalTo(expectedCells));
         assertThat(cellsLoser,equalTo(null));
     }
-    
+    //P1 AND P2 EMPATE
+    @Test
+    public void getCellsIfWinnerP1AndP2DrawTest() {
+    	//given
+        //dato dado
+        /* X 0 X
+         * 0 X X
+         * 0 X 0
+         * */
+        asignarValores("XOXOXXOXO");
+        //when
+        int[]cellsWinner=tablero.getCellsIfWinner(labelP2);
+        int[]cellsLoser=tablero.getCellsIfWinner(labelP1);
+        //then        
+        assertThat(cellsWinner, equalTo(null));
+        assertThat(cellsLoser,equalTo(null));
+    }
+    //TEST SI PASO EL MISMO STRING PERO SIN USAR EL LABEL
+    @Test
+    public void withoutLabelTest() {
+    	//given
+        /* 0 0 0
+           X X 0
+           X 0 X
+         */    	
+    	String table="OOOXXOXOX";
+        asignarValores(table);
+        int[]expectedCells= {0,1,2};
+        //when
+        int[]cellsWinner=tablero.getCellsIfWinner("O");
+        int[]cellsLoser=tablero.getCellsIfWinner("X");
+        //then        
+        assertThat(cellsWinner, equalTo(expectedCells));
+        assertThat(cellsLoser,equalTo(null));
+    }
     private void asignarValores(String letras) {
         for (int i = 0; i < letras.length(); i++) {
             if(letras.charAt(i)=='X') {
