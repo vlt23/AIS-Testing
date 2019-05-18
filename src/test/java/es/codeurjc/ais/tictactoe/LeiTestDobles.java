2d4f684091b749ad4dfe4f0772b1303149b20774
vlt23 <vlt23@protonmail.com>
2019-05-18 12:40:12 +0200

    Cleanup code

--- src/test/java/es/codeurjc/ais/tictactoe/LeiTestDobles.java
+++ src/test/java/es/codeurjc/ais/tictactoe/LeiTestDobles.java
@@ -1,53 +1,54 @@
 package es.codeurjc.ais.tictactoe;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
+import es.codeurjc.ais.tictactoe.TicTacToeGame.Event;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import es.codeurjc.ais.tictactoe.TicTacToeGame.Event;
+import static org.mockito.Mockito.*;
 
 public class LeiTestDobles {
-	private TicTacToeGame juego;
-	private Connection connection1,connection2;
-	private Player player1,player2;
+    private TicTacToeGame game;
+    private Connection connection1, connection2;
+    private Player player1, player2;
+
     @Before
     public void setUp() {
-    	//given
-        juego=new TicTacToeGame();
-        connection1 =mock(Connection.class);
-        connection2 =mock(Connection.class);    
+        // given
+        game = new TicTacToeGame();  // 1
+        // 2
+        connection1 = mock(Connection.class);
+        connection2 = mock(Connection.class);
     }
 
     @Test
     public void checkFlujoEvento() {
-        //given
-    	Event event = new Event();
-    	event.type=TicTacToeGame.EventType.JOIN_GAME;
-    	//doNothing().when(connection1).sendEvent(event);  
-    	juego.addConnection(connection1);
-    	juego.addConnection(connection2);    	    	    	
-    	
-    	player1=new Player(0, "X", "a");
-    	player2=new Player(1, "O", "b");    	    
-    	
-    	List<Object> l=new ArrayList<Object>();
-    	l.add(player1);
-    	l.add(player2);
-    	//when
-    	juego.addPlayer(player1);
-    	juego.addPlayer(player2);
-    	
-    	//then    	
-    	verify(connection1,times(2)).sendEvent(TicTacToeGame.EventType.JOIN_GAME, l);
-    	verify(connection2,times(2)).sendEvent(TicTacToeGame.EventType.JOIN_GAME, l);
-    	
+        // given
+        Event event = new Event();
+        event.type = TicTacToeGame.EventType.JOIN_GAME;
+        //doNothing().when(connection1).sendEvent(event);
+        // 3
+        game.addConnection(connection1);
+        game.addConnection(connection2);
+
+        // 4
+        player1 = new Player(0, "X", "a");
+        player2 = new Player(1, "O", "b");
+
+        List<Object> l = new ArrayList<>();
+        l.add(player1);
+        l.add(player2);
+        // when
+        // 5
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+
+        // then
+        verify(connection1, times(2)).sendEvent(TicTacToeGame.EventType.JOIN_GAME, l);  // 6
+        verify(connection2, times(2)).sendEvent(TicTacToeGame.EventType.JOIN_GAME, l);  // 7
+
     }
 
 }
