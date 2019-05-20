9d5fc68aa9c2fdf5d72836c49ea22c03f8b3aa77
vlt23 <vlt23@protonmail.com>
2019-05-20 18:05:00 +0200

    Refactor SistemaTest to prevent duplicate code

--- src/test/java/es/codeurjc/ais/tictactoe/ValenSistemaTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/ValenSistemaTest.java
@@ -14,8 +14,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValenSistemaTest {
 
-    private WebDriver driverLei;
-    private WebDriver driverValen;
+    private WebDriver driver1;
+    private WebDriver driver2;
+
+    private String player1 = "Lei";
+    private String player2 = "Valen";
+
+    private String nickname = "nickname";
+    private String startBtn = "startBtn";
 
     @BeforeClass
     public static void setupClass() {
@@ -30,58 +36,53 @@ public class ValenSistemaTest {
 
     @Before
     public void setupTest() {
-        driverLei = new FirefoxDriver();
-        driverValen = new FirefoxDriver();
+        driver1 = new FirefoxDriver();
+        driver2 = new FirefoxDriver();
+
+        // Add connection
+        String url = "http://127.0.0.1:8080";
+        driver1.get(url);
+        driver2.get(url);
+
+        // Join game
+        driver1.findElement(By.id(nickname)).sendKeys(player1);
+        driver1.findElement(By.id(startBtn)).click();
+        driver2.findElement(By.id(nickname)).sendKeys(player2);
+        driver2.findElement(By.id(startBtn)).click();
     }
 
     @After
     public void teardown() {
-        if (driverLei != null) {
-            driverLei.quit();
+        if (driver1 != null) {
+            driver1.quit();
         }
-        if (driverValen != null) {
-            driverValen.quit();
+        if (driver2 != null) {
+            driver2.quit();
         }
     }
 
     @Test
-    public void winTest() throws InterruptedException {
-        String url = "http://127.0.0.1:8080";
-        driverLei.get(url);
-        driverValen.get(url);
-
-        String player1 = "Lei";
-        String player2 = "Valen";
-
-        String nickname = "nickname";
-        String startBtn = "startBtn";
-
-        driverLei.findElement(By.id(nickname)).sendKeys(player1);
-        driverLei.findElement(By.id(startBtn)).click();
-        driverValen.findElement(By.id(nickname)).sendKeys(player2);
-        driverValen.findElement(By.id(startBtn)).click();
-
+    public void player1WinTest() throws InterruptedException {
         /* X O X
            O X O
            X
          */
-
         String cell_x = "cell-";
         for (int i = 0; i < 7; i++) {
             if (i % 2 == 0) {
-                driverLei.findElement(By.id(cell_x + i)).click();
+                driver1.findElement(By.id(cell_x + i)).click();
             } else {
-                driverValen.findElement(By.id(cell_x + i)).click();
+                driver2.findElement(By.id(cell_x + i)).click();
             }
         }
 
-        String winMessage = "Lei wins! Valen looses.";
+        String winMessage = player1 + " wins! " + player2 + " looses.";
 
-        String leiMessage = driverLei.switchTo().alert().getText();
-        String valenMessage = driverValen.switchTo().alert().getText();
+        String p1Message = driver1.switchTo().alert().getText();
+        String p2Message = driver2.switchTo().alert().getText();
 
-        assertThat(winMessage).isEqualTo(leiMessage);
-        assertThat(winMessage).isEqualTo(valenMessage);
+        assertThat(winMessage).isEqualTo(p1Message);
+        assertThat(winMessage).isEqualTo(p2Message);
 
         Thread.sleep(5000);
     }
